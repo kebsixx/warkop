aaa4ac5c51cc89aa58ce91ddddec49917d0f3ebd
kebsixx <rijikanw@gmail.com>
2024-05-24 18:30:13 +0700

    add search form

--- js/script.js
+++ js/script.js
@@ -1,13 +1,30 @@
+// toggle active hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
 
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-const hamburger = document.querySelector("#hamburger-menu");
+// toggle active search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
 
-document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+document.querySelector("#search-button").onclick = (event) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  event.preventDefault();
+};
+
+// klik diluar eleemen
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
+
+document.addEventListener("click", function (event) {
+  if (!hm.contains(event.target) && !navbarNav.contains(event.target)) {
     navbarNav.classList.remove("active");
+  }
+
+  if (!sb.contains(event.target) && !searchForm.contains(event.target)) {
+    searchForm.classList.remove("active");
   }
 });
