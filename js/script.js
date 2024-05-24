d9672739971b4d4b2fe1b00639b0ebeae66f3dd2
kebsixx <rijikanw@gmail.com>
2024-05-24 19:52:44 +0700

    add shopping cart

--- js/script.js
+++ js/script.js
@@ -15,16 +15,27 @@ document.querySelector("#search-button").onclick = (event) => {
   event.preventDefault();
 };
 
-// klik diluar eleemen
+// toggle active shopping cart
+const shoppingCart = document.querySelector(".shopping-cart");
+
+document.querySelector("#shopping-cart-button").onclick = (event) => {
+  shoppingCart.classList.toggle("active");
+  event.preventDefault();
+}
+
+// klik diluar elemen
 const hm = document.querySelector("#hamburger-menu");
 const sb = document.querySelector("#search-button");
+const sc = document.querySelector("#shopping-cart-button");
 
 document.addEventListener("click", function (event) {
   if (!hm.contains(event.target) && !navbarNav.contains(event.target)) {
     navbarNav.classList.remove("active");
   }
-
   if (!sb.contains(event.target) && !searchForm.contains(event.target)) {
     searchForm.classList.remove("active");
+  }
+  if (!sc.contains(event.target) && !shoppingCart.contains(event.target)) {
+    shoppingCart.classList.remove("active");
   }
 });
