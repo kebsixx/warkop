c7f4ff20e9d07be71f0e3a14f1ffb25e51881cd5
kebsixx <rijikanw@gmail.com>
2024-06-11 15:15:42 +0700

    add intl js

--- js/script.js
+++ js/script.js
@@ -63,3 +63,12 @@ window.onclick = (event) => {
     itemDetailModal.style.display = "none";
   }
 };
+
+// konversi ke rupiah
+const rupiah = (number) => {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    minimumFractionDigits: 0,
+  }).format(number);
+}
